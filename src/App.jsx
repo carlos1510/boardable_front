abc0fc5c80c6025c7f8deb903bbc4f9824304bf2
carlos1510 <carloscle1510@gmail.com>
2024-02-06 21:49:28 -0500

    add configuration

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import * as React from 'react'
+
+function App() {
+  const [message, setMessage] = React.useState({})
+
+  React.useEffect(() => {
+    fetch("http://localhost:5500/")
+    .then((res) => res.json())
+    .then((data) => setMessage(data));
+  }, [])
+
+  return (
+    <div>
+      <h1>Test de hola mundo</h1>
+      <pre>
+        { JSON.stringify(message, null, 2) }
+      </pre>
+    </div>
+  )
+}
+
+export default App
