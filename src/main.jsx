10932b71d1067692a2546005d5f24e566d48d2e9
carlos1510 <carloscle1510@gmail.com>
2024-02-08 11:49:35 -0500

    agregar modulo de login

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import "./index.css";
 import App from './App.jsx'
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>
 )
